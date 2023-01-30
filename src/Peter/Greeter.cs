09b9707eeb184625f7e5219ec9ada4bdbbed67c9
Sergio León <panicoenlaxbox@gmail.com>
2023-01-30 15:58:11 +0100

    Initial commit

---
+++ src/Peter/Greeter.cs
@@ -0,0 +1,10 @@
+﻿namespace Peter
+{
+    public class Me
+    {
+        public string Hello(string name)
+        {
+            return $"Hello {name}, I'm Peter.";
+        }
+    }
+}
---
+++ tests/Peter.Tests/PeterShould.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Peter.Tests;
+
+public class PeterShould
+{
+    [Fact]
+    public void greet()
+    {
+        var peter = new Me();
+        peter.Hello("Sergio").Should().Be("Hello Sergio, I'm Peter.");
+    }
+}
