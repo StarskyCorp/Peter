0c6e5d4b59a74e94ed37697906a9948eb6df72fa
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2023-02-01 10:34:19 +0100

    Added server fixture and support for integration testing with secured endpoints

---
+++ src/Peter/Testing/TestAuthenticationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Peter.Testing;
+
+public static class TestAuthenticationExtensions
+{
+    public static AuthenticationBuilder AddTestAuthentication(this IServiceCollection services)
+    {
+        return services.AddAuthentication(defaultScheme: TestConstants.Authentication.TestScheme)
+            .AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>(
+                TestConstants.Authentication.TestScheme, options => { });
+    }
+
+    public static HttpClient WithIdentity(this HttpClient httpClient, IEnumerable<Claim> claims)
+    {
+        var serializedClaims = ClaimsSerializer.Encode(claims);
+
+        httpClient.DefaultRequestHeaders.Add(
+            name: TestConstants.Authentication.HeaderName,
+            value: $"{TestConstants.Authentication.TestScheme} {serializedClaims}");
+
+        return httpClient;
+    }
+}
---
+++ tests/GreetingsApi/Program.cs
@@ -0,0 +1,13 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
+WebApplication app = builder.Build();
+
+app.MapGet("/{who}", (string who) => $"Hello {who}!");
+app.MapGet("/{who}/Authenticated", (string who, HttpContext context) => $"Hello {who}! Here are your claims: {string.Join(",", context.User.Claims)}").RequireAuthorization();
+
+app.Run();
+
+public partial class Program { } //Test concession
