ccb3929324cfdbe596d2d04fe4e90bc08ec83ee8
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2023-02-01 10:34:19 +0100

    Added server fixture and support for integration testing with secured endpoints

---
+++ src/Peter/Testing/TestConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Peter.Testing;
+
+public static class TestConstants
+{
+    internal static class Authentication
+    {
+        public const string TestScheme = "TestScheme";
+        public const string TestAuthType = "TestAuthType";
+        public static string HeaderName => $"X-TestServerAuthentication-{TestScheme}";
+    }
+}
