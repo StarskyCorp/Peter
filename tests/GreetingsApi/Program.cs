8679116923fb8be4aedc3be2cf33505baf2f3e98
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2023-02-02 08:31:55 +0100

    Boy scout

--- tests/GreetingsApi/Program.cs
+++ tests/GreetingsApi/Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddAuthorization();
 WebApplication app = builder.Build();
 
 app.MapGet("/{who}", (string who) => $"Hello {who}!");
-app.MapGet("/{who}/Authenticated", (string who, HttpContext context) => $"Hello {who}! Here are your claims: {string.Join(",", context.User.Claims)}").RequireAuthorization();
+app.MapGet("/{who}/Authenticated", (string who, HttpContext context) => $"Hello {who}! Here are your claims: {string.Join(",", context.User.Claims)}")
+    .RequireAuthorization();
 
 app.Run();
 
