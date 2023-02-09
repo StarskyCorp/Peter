96ce3e726de5eef4058a73a75cc5e001587cfc71
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2023-02-09 18:40:09 +0100

    Renamin folder

--- tests/Peter.Result.Tests/ResultShould.cs
+++ tests/Peter.Result.Tests/ResultShould.cs
@@ -5,12 +5,12 @@ namespace Peter.Result.Tests;
 
 public class ResultShould
 {
-    private static readonly Fixture Fixture = new();
+    private static readonly Fixture _fixture = new();
 
     [Fact]
     public void create_success_result()
     {
-        var result = Result<object>.CreateSuccess(Fixture.Create<object>());
+        var result = Result<object>.CreateSuccess(_fixture.Create<object>());
         result.Success.Should().BeTrue();
         result.Value.Should().NotBeNull();
         result.Status.Should().Be(ResultStatus.Success);
@@ -28,7 +28,7 @@ public class ResultShould
     [Fact]
     public void create_failed_result()
     {
-        var result = Result<object>.CreateFailure(Fixture.Create<IEnumerable<string>>());
+        var result = Result<object>.CreateFailure(_fixture.Create<IEnumerable<string>>());
         result.Success.Should().BeFalse();
         result.Value.Should().BeNull();
         result.Status.Should().Be(ResultStatus.Failure);
@@ -38,7 +38,7 @@ public class ResultShould
     [Fact]
     public void create_failed_result_with_value()
     {
-        var result = Result<object>.CreateFailure(Fixture.Create<IEnumerable<string>>(), Fixture.Create<object>());
+        var result = Result<object>.CreateFailure(_fixture.Create<IEnumerable<string>>(), _fixture.Create<object>());
         result.Success.Should().BeFalse();
         result.Value.Should().NotBeNull();
         result.Status.Should().Be(ResultStatus.Failure);
@@ -57,7 +57,7 @@ public class ResultShould
     [Fact]
     public void create_not_existing_result_with_value()
     {
-        var result = Result<object>.CreateNotExists(Fixture.Create<object>());
+        var result = Result<object>.CreateNotExists(_fixture.Create<object>());
         result.Success.Should().BeFalse();
         result.Value.Should().NotBeNull();
         result.Status.Should().Be(ResultStatus.NotExists);
@@ -78,7 +78,7 @@ public class ResultShould
     public void create_invalid_result_with_value()
     {
         var validationErrors = new List<ValidationError> { new(field: "Name", message: "Mandatory") };
-        var result = Result<object>.CreateInvalid(validationErrors, Fixture.Create<object>());
+        var result = Result<object>.CreateInvalid(validationErrors, _fixture.Create<object>());
         result.Success.Should().BeFalse();
         result.Value.Should().NotBeNull();
         result.Status.Should().Be(ResultStatus.Invalid);
@@ -88,21 +88,21 @@ public class ResultShould
     [Fact]
     public void convert_to_bool_from_successful_result()
     {
-        var result = Result<object>.CreateSuccess(Fixture.Create<object>());
+        var result = Result<object>.CreateSuccess(_fixture.Create<object>());
         ((bool)result).Should().BeTrue();
     }
 
     [Fact]
     public void convert_to_bool_from_failed_result()
     {
-        var result = Result<object>.CreateFailure(Fixture.Create<IEnumerable<string>>());
+        var result = Result<object>.CreateFailure(_fixture.Create<IEnumerable<string>>());
         ((bool)result).Should().BeFalse();
     }
 
     [Fact]
     public void convert_to_successful_result_from_any_value()
     {
-        var value = Fixture.Create<string>();
+        var value = _fixture.Create<string>();
         Result<string> result = value;
         result.Success.Should().BeTrue();
         result.Value.Should().Be(value);
