8d860ce7086dac7c582be715bab4b83ca1f7c86c
Sergio Navarro Pino <sergio.navarropino@gmail.com>
2023-02-22 11:31:38 +0100

    Updated packages

---
+++ src/Peter.MinimalApi.Testing/XUnitLogger.cs
@@ -0,0 +1,46 @@
+﻿//using Microsoft.Extensions.Logging;
+//using Xunit.Abstractions;
+
+//namespace Peter.MinimalApi.Testing;
+
+//public class XUnitLogger : ILogger
+//{
+//    private readonly ITestOutputHelper _testOutputHelper;
+//    private readonly string _categoryName;
+
+//    public XUnitLogger(ITestOutputHelper testOutputHelper, string categoryName)
+//    {
+//        _testOutputHelper = testOutputHelper;
+//        _categoryName = categoryName;
+//    }
+
+//    public IDisposable BeginScope<TState>(TState state) => NoopDisposable.Instance;
+
+//    public bool IsEnabled(LogLevel logLevel) => true;
+
+//    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
+//        Func<TState, Exception, string> formatter)
+//    {
+//        try
+//        {
+//            var message = $"{_categoryName} [{eventId}]\n{formatter(state, exception)}";
+//            _testOutputHelper.WriteLine(message);
+//            if (exception is not null)
+//            {
+//                _testOutputHelper.WriteLine(exception.ToString());
+//            }
+//        }
+//        catch (InvalidOperationException)
+//        {
+//            // Prevent System.InvalidOperationException: 'There is no currently active test.'
+//            // BeforeAfterTestAttribute has an active test, but ICollectionFixture<> does not have
+//        }
+//    }
+
+//    private class NoopDisposable : IDisposable
+//    {
+//        public static readonly NoopDisposable Instance = new();
+
+//        public void Dispose() {}
+//    }
+//}
---
+++ src/Peter.MinimalApi.Testing/XUnitLoggerProvider.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.Extensions.Logging;
+//using Xunit.Abstractions;
+
+//namespace Peter.MinimalApi.Testing;
+
+//public class XUnitLoggerProvider : ILoggerProvider
+//{
+//    private readonly ITestOutputHelper _testOutputHelper;
+
+//    public XUnitLoggerProvider(ITestOutputHelper testOutputHelper) => _testOutputHelper = testOutputHelper;
+
+//    public ILogger CreateLogger(string categoryName) => new XUnitLogger(_testOutputHelper, categoryName);
+
+//    public void Dispose() {}
+//}
