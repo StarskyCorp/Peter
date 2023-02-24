b026d07840e4585b493840acf63725d25e65d46e
Sergio León <panicoenlaxbox@gmail.com>
2023-02-24 10:54:03 +0100

    Changed local packages script

--- CreateLocalPackages.ps1
+++ CreateLocalPackages.ps1
@@ -1,3 +1,10 @@
-New-Item -Path "C:\PeterLocalPackages" -ItemType Directory -ErrorAction SilentlyContinue
-dotnet nuget add source C:\PeterLocalPackages --name PeterLocal  | Out-Null
-dotnet pack Peter.sln -c Debug -o C:\PeterLocalPackages --version-suffix "-local"
+$path = "C:\PeterLocalPackages"
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -ItemType Directory
+}
+$source = "PeterLocal"
+if (dotnet nuget list source | Where-Object { $_ -like "*$source*" }) {
+    dotnet nuget remove source $source
+}
+dotnet nuget add source $path --name $source
+dotnet pack Peter.sln -c Debug -o $path --version-suffix "-local"
